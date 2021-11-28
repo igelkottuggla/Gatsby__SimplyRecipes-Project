f785516851d671ee2896968893190adedc5dbf4f
NataliaSova <rot.fuchslein@gmail.com>
2021-11-28 21:12:03 +0500

    starting structure

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
 
-export default function Home() {
+const about = () => {
   return (
     <Layout>
-      <div>Hello people!!!</div>
+      <div>hello from about</div>
     </Layout>
   )
 }
+
+export default about
