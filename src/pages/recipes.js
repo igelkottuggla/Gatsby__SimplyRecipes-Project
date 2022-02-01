e7bdf74992ed5636c21546e69b5736888f5a6be5
NataliaSova <rot.fuchslein@gmail.com>
2022-02-01 19:33:20 +0500

    Contentful CMS, Recipes list, tags list, starting recipe template page setup

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,11 +1,12 @@
 import React from "react"
 import Layout from "../components/Layout"
+import AllRecipes from "../components/AllRecipes"
 
 const Recipes = () => {
   return (
     <Layout>
       <main className="page">
-        <h1>recipes page</h1>
+        <AllRecipes />
       </main>
     </Layout>
   )
